71b75849a7da60f050038f9219d952904cac5c72
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-03-20 21:18:25 +0500

    Update remove microsoft store.ps1

--- remove microsoft store.ps1
+++ remove microsoft store.ps1
@@ -1,2 +1,6 @@
 Get-AppxPackage -AllUsers *StorePurchaseApp | Remove-AppxPackage
 Get-AppxPackage -AllUsers *Store | Remove-AppxPackage
+//////
+//////
+//////
+Get-AppxPackage *windowsstore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppxManifest.xml"}
