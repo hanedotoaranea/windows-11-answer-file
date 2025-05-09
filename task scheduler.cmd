dd02861444b58eebdf7c7453230f6d6de9c05ea2
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-09 14:17:02 +0500

    Add files via upload

---
+++ task scheduler.cmd
@@ -0,0 +1,7 @@
+schtasks /delete /tn "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /f
+schtasks /delete /tn "\Microsoft\Windows\Customer Experience Improvement Program\KernelCeipTask" /f
+schtasks /delete /tn "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /f
+schtasks /delete /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /f
+schtasks /delete /tn "\Microsoft\Windows\Application Experience\ProgramDataUpdater" /f
+schtasks /delete /tn "\Microsoft\Windows\Autochk\Proxy" /f
+schtasks /delete /tn "\Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" /f
