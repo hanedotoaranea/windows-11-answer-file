56ae9752585941f05d99995baab7724bcd0812ef
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-13 17:16:55 +0500

    Update Explorer.cmd

--- Custom/Explorer.cmd
+++ Custom/Explorer.cmd
@@ -21,7 +21,7 @@ reg add "HKCU\Control Panel\Desktop" /v HungAppTimeout /t REG_SZ /d "1000" /f
 reg add "HKCU\Control Panel\Desktop" /v WaitToKillAppTimeout /t REG_SZ /d "2000" /f
 reg add "HKCU\Control Panel\Desktop" /v AutoEndTasks /t REG_SZ /d "1" /f
 reg add "HKCU\Control Panel\Desktop" /v LowLevelHooksTimeout /t REG_SZ /d "1000" /f
-
+reg add "HKLM\SYSTEM\CurrentControlSet\Control" /v "WaitToKillServiceTimeout" /t REG_SZ /d "2000" /f
 
 
 
