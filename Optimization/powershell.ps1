86d67e9635c6bf57f6f808e1c348be67657dfe23
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-13 17:16:42 +0500

    Update powershell.ps1

--- Optimization/powershell.ps1
+++ Optimization/powershell.ps1
@@ -3,3 +3,5 @@ Get-AppxPackage -AllUsers *Store | Remove-AppxPackage
 Get-AppxPackage -AllUsers *Windows.DevHome* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *windowsphone* | Remove-AppxPackage
 Get-AppxPackage -AllUsers *xboxapp* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage
+
