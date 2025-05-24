9d5e08103d350e3cfe49f4f17d41c680e44a5df7
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-24 12:25:46 +0500

    Delete Windows 11 directory

--- Windows 11/apps.ps1
+++
@@ -1,5 +0,0 @@
-Get-AppxPackage -AllUsers *StorePurchaseApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Store | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Windows.DevHome* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *windowsphone* | Remove-AppxPackage
-Get-AppxPackage -AllUsers *xboxapp* | Remove-AppxPackage
