fa54d0ba594c63ff9d1e2a7706bd3376da24bcab
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-24 12:09:45 +0500

    Delete Windows 10/Optimization/remove microsoft store.ps1

--- Windows 10/Optimization/remove microsoft store.ps1
+++
@@ -1,6 +0,0 @@
-Get-AppxPackage -AllUsers *StorePurchaseApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers *Store | Remove-AppxPackage
-//////
-//////
-//////
-Get-AppxPackage *windowsstore* -AllUsers | Foreach {Add-AppxPackage -DisableDevelopmentMode -Register "$($_.InstallLocation)\AppxManifest.xml"}
