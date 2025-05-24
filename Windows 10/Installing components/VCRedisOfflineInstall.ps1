e96da01beb85be2cc5ee41eb3a2fe080bfafed65
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-05-24 12:26:27 +0500

    Delete Windows 10/Installing components directory

--- Windows 10/Installing components/VCRedisOfflineInstall.ps1
+++
@@ -1,9 +0,0 @@
-winget download Microsoft.VCRedist.2013.x64 -s winget
-winget download Microsoft.VCRedist.2013.x86 -s winget
-winget download Microsoft.VCRedist.2012.x86 -s winget
-winget download Microsoft.VCRedist.2010.x86 -s winget
-winget download Microsoft.VCRedist.2010.x64 -s winget
-winget download Microsoft.VCRedist.2015+.x64 -s winget
-winget download Microsoft.VCRedist.2015+.x86 -s winget
-winget download Microsoft.VCRedist.2008.x64 -s winget
-winget download Microsoft.VCRedist.2008.x86 -s winget
--- Windows 10/Installing components/winget.ps1
+++
@@ -1,6 +0,0 @@
-winget install --id=hwinfo -e
-winget install --id=Valve.Steam -e
-winget install --id=Google.Chrome -e
-winget install --id=Transmission.Transmission  -e
-winget install --id=PeterPawlowski.foobar2000 -e
-winget install --id=VideoLAN.VLC  -e
