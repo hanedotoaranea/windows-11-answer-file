94fed163fd08b6f776f4fb5b7c29881e016da28a
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-08-24 14:05:20 +0500

    Add files via upload
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

---
+++ PowerShell/Удаление из планировщика задач, связанных с телеметрией.ps1
@@ -0,0 +1,6 @@
+schtasks /delete /tn "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator" /f
+schtasks /delete /tn "\Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" /f
+schtasks /delete /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" /f
+schtasks /delete /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser Exp" /f
+schtasks /delete /tn "\Microsoft\Windows\Application Experience\StartupAppTask" /f
+schtasks /delete /tn "\Microsoft\Windows\Windows Error Reporting\QueueReporting" /f
