3a89395d6cc6d98ccd464094576c8ad833cdd0ec
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-08-25 20:52:30 +0500

    Update deleteedge.ps1
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- Edge/deleteedge.ps1
+++ Edge/deleteedge.ps1
@@ -4,3 +4,9 @@ Remove-Item -Path "EdgeCore" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "EdgeUpdate" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "EdgeWebView" -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -Path "Temp" -Recurse -Force -ErrorAction SilentlyContinue
+
+# Найти точное имя пакета Edge
+Get-WindowsPackage -Online | Where-Object {$_.PackageName -like "*Edge*"} | Format-List PackageName
+
+# Удалить (пример)
+DISM /Online /Remove-Package /PackageName:Microsoft-Windows-Edge-Browser-Package~31bf3856ad364e35~amd64~~10.0.19041.1266 /NoRestart
