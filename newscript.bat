2ffd965231635e723430a39f1ff1eb6be540f246
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2026-05-05 12:36:53 +0500

    Update newscript.bat
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- newscript.bat
+++ newscript.bat
@@ -1,5 +1,15 @@
+
+# 3. НАСТРОЙКИ ИНТЕРФЕЙСА
+REG DELETE "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" /f
+reg add "HKCU\Control Panel\Desktop" /v HungAppTimeout /t REG_SZ /d "1000" /f
+reg add "HKCU\Control Panel\Desktop" /v ForegroundFlashCount /t REG_DWORD /d 9999 /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSecondsInSystemClock /t REG_DWORD /d 1 /
+# 4. ПЛАНИРОВЩИК (ПОЛЬЗОВАТЕЛЬСКИЕ ЗАДАЧИ)
+schtasks /change /tn "Microsoft\Windows\MemoryDiagnostic\ProcessMemoryDiagnosticEvents" /disable 2>$null
+schtasks /change /tn "Microsoft\Windows\MemoryDiagnostic\RunFullMemoryDiagnostic" /disable 2>$null
 # Настройка времени отключения монитора в параметрах схемы электропитания
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
+
 sc.exe config AMD Crash Defender Service start= disabled
 sc.exe config AMD External Events Utility start= disabled
 sc.exe config DoSvc  start= disabled
