c1501454f79e2fbde44eb63ff4e066ea94cc2797
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2026-05-05 12:36:28 +0500

    Update newscript.bat
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- newscript.bat
+++ newscript.bat
@@ -1,16 +1,15 @@
 # Настройка времени отключения монитора в параметрах схемы электропитания
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
-
-AMD Crash Defender Service
-AMD External Events Utility  
-DoSvc 
-sc delete diagsvc
-WdiSystemHost
+sc.exe config AMD Crash Defender Service start= disabled
+sc.exe config AMD External Events Utility start= disabled
+sc.exe config DoSvc  start= disabled
+sc delete diagsvc start= disabled
+sc.exe config WdiSystemHost start= disabled
+sc.exe config TrkWks start= disabled
+sc.exe config lfsvc start= disabled
+sc.exe config RasMan start= disabled
+sc.exe config SSDPSRV start= disabled
+sc.exe config DiagTrack start= disabled
+sc.exe stop DiagTrack 
 sc delete DiagTrack
-TrkWks
-lfsvc
-RasMan
-SSDPSRV
-
-
 
