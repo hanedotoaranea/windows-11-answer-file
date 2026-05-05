628fd2e573990fb404db57edf2332081711a416f
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2026-05-05 12:38:02 +0500

    Update newscript.bat
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- newscript.bat
+++ newscript.bat
@@ -11,15 +11,25 @@ schtasks /change /tn "Microsoft\Windows\MemoryDiagnostic\RunFullMemoryDiagnostic
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\7516b95f-f776-4464-8c53-06167f40cc99\8EC4B3A5-6868-48c2-BE75-4F3044BE88A7" /v Attributes /t REG_DWORD /d 2 /f
 
 sc.exe config AMD Crash Defender Service start= disabled
+sc.exe stop AMD Crash Defender Service
 sc.exe config AMD External Events Utility start= disabled
+sc.exe stop AMD External Events Utility
 sc.exe config DoSvc  start= disabled
+sc.exe stop DoSvc
 sc delete diagsvc start= disabled
+sc.exe stop diagsvc
 sc.exe config WdiSystemHost start= disabled
+sc.exe stop WdiSystemHost
 sc.exe config TrkWks start= disabled
+sc.exe stop TrkWks
 sc.exe config lfsvc start= disabled
+sc.exe stop lfsvc
 sc.exe config RasMan start= disabled
+sc.exe stop RasMan
 sc.exe config SSDPSRV start= disabled
+sc.exe stop SSDPSRV
 sc.exe config DiagTrack start= disabled
+sc.exe stop DiagTrack
 sc.exe stop DiagTrack 
 sc delete DiagTrack
 
