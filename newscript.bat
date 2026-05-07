9e4fe22ed10314ee3cd13fc47285753a0d37d57c
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2026-05-07 21:27:48 +0500

    Update newscript.bat
    Signed-off-by: hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>

--- newscript.bat
+++ newscript.bat
@@ -49,53 +49,48 @@ reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Dsh" /v "AllowWidgets" /t
 
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v SystemResponsiveness /t REG_DWORD /d 20 /f
-увеличивает приоритет игровых и мультимедийных задач
+# увеличивает приоритет игровых и мультимедийных задач
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Multimedia\SystemProfile" /v "NetworkThrottlingIndex" /t REG_DWORD /d -1 /f
-отключает ограничение пропускной способности сети
+# отключает ограничение пропускной способности сети
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v "Win32PrioritySeparation" /t REG_DWORD /d 00000026 /f
-оптимизирует распределение ресурсов процессора, отдавая максимальный приоритет активному окну
+# оптимизирует распределение ресурсов процессора, отдавая максимальный приоритет активному окну
 reg add "HKLM\SYSTEM\CurrentControlSet\Services\Tcpip\Parameters" /v DefaultTTL /t REG_DWORD /d 128 /f
-стандартное время жизни (TTL) для исходящих IP-пакетов равным 128.
+# стандартное время жизни (TTL) для исходящих IP-пакетов равным 128.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
-полностью отключает сбор телеметрии
+# полностью отключает сбор телеметрии
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" /v "MaintenanceDisabled" /t REG_DWORD /d 1 /f
-отключает Automatic Maintenance
+# отключает Automatic Maintenance
 reg add "HKLM\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection" /v "AllowTelemetry" /t REG_DWORD /d 0 /f
-полностью отключает сбор телеметрии 32-битных приложений
+# полностью отключает сбор телеметрии 32-битных приложений
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\DataCollection" /v "DoNotShowFeedbackNotifications" /t REG_DWORD /d 1 /f
-отключает назойливые уведомления с просьбой оставить отзыв о работе Windows.
+# отключает назойливые уведомления с просьбой оставить отзыв о работе Windows.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "EnableActivityFeed" /t REG_DWORD /d 0 /f
-
-отключает «Журнал действий»
-
+# отключает «Журнал действий»
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo" /v "Enabled" /t REG_DWORD /d 0 /f
- отключения рекламного идентификатора
-
+# отключения рекламного идентификатора
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "AllowClipboardHistory" /t REG_DWORD /d 0 /f
-полностью отключает облачный буфер обмена
+# полностью отключает облачный буфер обмена
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "PublishUserActivities" /t REG_DWORD /d 0 /f
- запрещает публикацию действий пользователя в облаке.
+# запрещает публикацию действий пользователя в облаке.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "UploadUserActivities" /t REG_DWORD /d 0 /f
-запрещает выгрузку (upload) вашей активности на серверы Microsoft.
+# запрещает выгрузку (upload) вашей активности на серверы Microsoft.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d 0 /f
-полностью отключает Cortana
+# полностью отключает Cortana
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "AITEnable" /t REG_DWORD /d 0 /f
- отключает сбор данных об использовании приложений
+# отключает сбор данных об использовании приложений
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\AppCompat" /v "DisableInventory" /t REG_DWORD /d 1 /f
- запрещает Windows сканировать компьютер для сбора списка установленного ПО и файлов.
-
+# запрещает Windows сканировать компьютер для сбора списка установленного ПО и файлов.
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
-полностью отключает функции записи экрана и захвата игр 
-
+# полностью отключает функции записи экрана и захвата игр 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Messenger\Client" /v "CEIP" /t REG_DWORD /d 2 /f
- Полностью отключает программу улучшения качества ПО
+# Полностью отключает программу улучшения качества ПО
 reg add "HKLM\SOFTWARE\Policies\Microsoft\SQMClient\Windows" /v "CEIPEnable" /t REG_DWORD /d 0 /f
- Полностью отключает программу улучшения качества ПО
-
+# Полностью отключает программу улучшения качества ПО
 reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v "NoAutoUpdate" /t REG_DWORD /d 1 /f
-полностью отключает автоматическое обновление Windows
-
-
+# полностью отключает автоматическое обновление Windows
+sc.exe config SSDPSRV start= disabled
+sc.exe stop SSDPSRV
+# отключает службу обнаружения SSDP
 
 
 
